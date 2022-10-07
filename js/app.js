28edf582136cccea459e6ac8e13af52f942386ee
Cinthya Escobar <cinthyacarolinaescobar@gmail.com>
2022-10-06 22:30:07 -0300

    agregue fetch

--- js/app.js
+++ js/app.js
@@ -1,43 +1,60 @@
-let productos = [{ id: 1, nombre: 'Delineador', marca: 'Maybelline', precio: 3000, imagen: "./imagenes/delinea.jpg", stock: 1 },
-{ id: 2, nombre: 'Base de maquillaje', marca: 'Loreal', precio: 6000, imagen: "./imagenes/base.jpg", stock: 1 },
-{ id: 3, nombre: 'Paleta de sombras', marca: 'NYX', precio: 10500, imagen: "./imagenes/paleta.jpg", stock: 1 },
-{ id: 4, nombre: 'Serum Acido Hialuronico', marca: 'Loreal', precio: 12000, imagen: "./imagenes/serum.jpg", stock: 1 },
-{ id: 5, nombre: 'Crema Hidratante', marca: 'Neutrogena', precio: 15000, imagen: "./imagenes/neutrogena.jpg", stock: 1 },
-{ id: 6, nombre: 'Set rubores', marca: 'MAC', precio: 8000, imagen: "./imagenes/rubor.jpg", stock: 1 },
-{ id: 7, nombre: 'Máscara de pestañas', marca: 'Maybelline', precio: 9000, imagen: "./imagenes/mascara.jpg", stock: 1 },
-{ id: 8, nombre: 'Set labiales', marca: 'Beauty Creations', precio: 13000, imagen: "./imagenes/labiales.jpg", stock: 1 }]
-
+const contadorCarrito = document.getElementById('contadorCarrito')
+const contenedorCarrito = document.getElementById('lista-carrito')
+const contenedorProductos = document.getElementById('contenedor-productos')
+const botonLimpiar = document.getElementById('limpiar-carrito')
+const botonComprar = document.getElementById('pagar-carrito')
 
 let carrito = []
+//lidiar con el carrito cuando es null, o no lo que esperas en tu app. (otro carrito(?))
+
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('carrito')) {
         carrito = JSON.parse(localStorage.getItem('carrito'))
-        actualizarCarrito()
+        if (carrito[0] === 'ID-3452') {
+            actualizarCarrito()
+        } else {
+            carrito = []
+        }
     }
 })
 
-const contadorCarrito = document.getElementById('contadorCarrito')
-const contenedorCarrito = document.getElementById('lista-carrito')
-const botonLimpiar = document.getElementById('limpiar-carrito')
-
-
-
-//funcion para mostrar mensaje de producto agregado
-function calcularAgregados() {
-    let agregado = ""
-    carrito.forEach((producto) => {
-        agregado = producto.nombre + ' \n Precio: $' + producto.precio
-    })
-    return agregado
+const agregarCarrito = (productoId) => {
+    if (carrito.length == 0) {
+        //le asigno un valor al carrito
+        carrito.push('ID-3452')
+    }
+    fetch("./data.json")
+        .then((resp) => resp.json())
+        .then((productos) => {
+            const existe = carrito.some(producto => producto.id === productoId)
+            if (existe) {
+                const producto = carrito.map(producto => {
+                    if (producto.id === productoId) {
+                        producto.stock++
+                        return producto
+                    }
+                })
+            } else {
+                const item = productos.find((producto) => producto.id === productoId)
+                carrito.push(item)
+            }
+            actualizarCarrito()
+        }
+        )
 }
 
-//dibujar productos
-const mostrarProductos = () => {
-    const contenedorProductos = document.getElementById("contenedor-productos")
-    productos.forEach((producto, indice) => {
-        let card = document.createElement("div")
-        card.classList.add("card", "col-sm-12", "col-lg-3")
-        card.innerHTML = `<img src = ${producto.imagen}  class=" card-img" alt=''>
+
+//fetch
+
+//MOSTRAR PRODUCTOS
+const productos = document.querySelector("#contenedor-productos")
+fetch("./data.json")
+    .then((resp) => resp.json())
+    .then((productos) => {
+        productos.forEach((producto) => {
+            let card = document.createElement("div")
+            card.classList.add("card", "col-sm-12", "col-lg-3")
+            card.innerHTML = `<img src = ${producto.imagen}  class=" card-img" alt=''>
         <div class="card-body">
         <h5 class = "card-title"> ${producto.nombre}</h5>
         <p class="card-text">$${producto.precio}</p>
@@ -45,53 +62,31 @@ const mostrarProductos = () => {
         <button id="agregar ${producto.id}" class= "btn btn-info boton-agregar"> Agregar al carrito</button>
         <br>
         </div>`
-        contenedorProductos.appendChild(card)
-        const boton = document.getElementById(`agregar ${producto.id}`)
-        boton.addEventListener('click', () => {
-            //envio id por parametro
-            agregarCarrito(producto.id)
-        })
-        //llamo funcion 
-        boton.addEventListener('click', () => {
-            Toastify({
-
-                text: (`Has agregado al carrito: ${calcularAgregados()} `),
-
-                duration: 3000,
-                style: {
-                    background: "linear-gradient(to right , #ff89b4, #ff3d83    )",
-                  }
-
-            }).showToast();
+            contenedorProductos.appendChild(card)
+            const boton = document.getElementById(`agregar ${producto.id}`)
+            boton.addEventListener('click', () => {
+                agregarCarrito(producto.id)
+            })
+            boton.addEventListener('click', () => {
+                Toastify({
+                    text: ('Producto agregado al carrito'),
+                    duration: 3000,
+                    style: {
+                        background: "linear-gradient(to right , #ff89b4, #ff3d83    )",
+                    }
+                }).showToast();
+            })
         })
     })
-}
-mostrarProductos()
 
-const agregarCarrito = (productoId) => {
-    //verifico si producto id ya esta agregado y si esta aumento cantidad stock
-    const existe = carrito.some(producto => producto.id === productoId)
-    if (existe) {
-        const producto = carrito.map(producto => {
-            if (producto.id === productoId) {
-                producto.stock++
-                return producto
-            }
-        })
-    } else {
-        const item = productos.find((producto) => producto.id === productoId)
-        carrito.push(item)
-    }
-    actualizarCarrito()
-}
 
-//limpiar contenido
+
+//limpiar contenido carrito
 botonLimpiar.addEventListener('click', () => {
     vaciarCarrito()
-
 })
 function vaciarCarrito() {
-    carrito.length >= 1 ? 
+    if (carrito.length > 1) {
         Swal.fire({
             title: "¿Quiere vaciar su carrito?",
             icon: "warning",
@@ -112,21 +107,87 @@ function vaciarCarrito() {
                     duration: 3000,
                     style: {
                         background: "linear-gradient(to right, #ff89b4  , #ff89b4  )",
-                      }
+                    }
                 }).showToast();
             }
-        }):
+        })
+    } else {
         Toastify({
-
             text: ('No tienes productos en el carrito'),
             duration: 3000,
-           
-        }).showToast();
-        
 
-    
+        }).showToast();
+    }
 }
-//eliminar productos
+
+botonComprar.addEventListener('click', () => {
+    comprarProducto()
+})
+
+
+function comprarProducto() {
+    if (carrito.length >= 1) {
+        Swal.fire({
+            title: "¿Desea realizar compra?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#f15c8e",
+            cancelButtonColor: "#d33",
+            cancelButtonText: "Cancelar",
+            confirmButtonText: "Confirmar",
+          
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Seleccione método de pago',
+                    text: (`La cantidad a pagar es de: $${precioTotal.innerHTML} `),
+                    width: '800px',
+                    showCancelButton: true,
+                    cancelButtonText: "Cancelar",
+                    cancelButtonColor: "#d33",
+                    input: 'radio',                       
+                    inputOptions: {
+                        '#ff0000': 'Tarjeta débito/crédito',
+                        '#00ff00': 'Pago en efectivo',
+                        
+
+                    },
+                    
+                    inputValidator: function(result) {
+                        if (!result) {
+                          return 'Seleccione una forma de pago';
+                        }
+                      }
+                }).then(function (result) {
+                    if (result.isConfirmed) {
+                        carrito.length = 0
+                        actualizarCarrito()
+                        contadorCarrito.innerText = 0
+                        localStorage.clear()
+                        carrito.length === 0 && Toastify({
+                            text: ('Has realizado tu pago. Gracias por su compra'),
+                            duration: 3000,
+                            style: {
+                                background: "linear-gradient(to right, #ff89b4  , #ff89b4  )",
+                            }
+                        }).showToast();
+                    }
+                })
+
+            }
+        })
+    } else {
+        Toastify({
+            text: ('No tienes productos en el carrito'),
+            duration: 3000,
+
+        }).showToast();
+    }
+}
+
+
+
+//eliminar productos carrito
 function eliminarProducto(productoId) {
     const item = carrito.find((producto) => producto.id === productoId)
     Swal.fire({
@@ -149,7 +210,7 @@ const eliminarDelCarrito = (productoId) => {
     carrito.splice(indice, 1)
     localStorage.removeItem("carrito");
     // cambiar contador cuando carrito llegue a 0
-    if (carrito.length === 0) {
+    if (carrito.length === 1) {
         contadorCarrito.innerHTML = 0
     }
     Toastify({
@@ -160,11 +221,12 @@ const eliminarDelCarrito = (productoId) => {
 
 }
 
+
+//actualizar productos carrito
 const actualizarCarrito = () => {
     //reseteo carrito
     contenedorCarrito.innerHTML = ""
-    carrito.forEach((producto) => {
-
+    carrito.slice(1).forEach((producto) => {
         const row = document.createElement('tr')
         row.innerHTML = `
         <img src=${producto.imagen} width="150" height=150";>
@@ -180,18 +242,19 @@ const actualizarCarrito = () => {
         localStorage.setItem('carrito', JSON.stringify(carrito))
     })
 
+
+
     //contar productos en carrito
     function calcularContador() {
         let contador = 0
-        carrito.forEach((producto) => {
+        carrito.slice(1).forEach((producto) => {
             contador += producto.stock
         })
         return contador
     }
 
     //calculo total 
-    const precioTotal = document.getElementById('precioTotal')
-    precioTotal.innerText = carrito.reduce((acc, producto) => acc + producto.stock * producto.precio, 0)
+    precioTotal.innerText = carrito.slice(1).reduce((acc, producto) => acc + producto.stock * producto.precio, 0)
 }
 
 //FORMULARIO
@@ -209,16 +272,15 @@ function formControl(event) {
     formData = new FormData(formulario)
 
     function validateForm() {
-        let input1 = document.forms["form"]["nombre"].value;
-        let input2 = document.forms["form"]["email"].value;
-
-        input1.trim() == "" || input2.trim() == "" ? Swal.fire({
-            position: 'top-end',
-            icon: 'error',
-            title: ('Debes ingresar tu nombre y tu email !'),
-            showConfirmButton: false,
-            timer: 1500
-        }) :
+        if (formData.get('nombre').trim() == "" || formData.get('email').trim() == "") {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: ('Debes ingresar tu nombre y tu email !'),
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } else {
             Swal.fire({
                 position: 'top-end',
                 icon: 'success',
@@ -226,6 +288,7 @@ function formControl(event) {
                 showConfirmButton: false,
                 timer: 2000
             })
+        }
     }
     validateForm()
 
